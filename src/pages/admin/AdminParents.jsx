c817928a300deac1ca370a3b5ee62977f914666e
nikhilbikkanoori <nikhilbikkanoori.com>
2025-12-08 08:51:08 +0530

    v1.9

--- src/pages/admin/AdminParents.jsx
+++ src/pages/admin/AdminParents.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 
-const API_BASE = "https://dropshieldbe-a3fmaucneacte4av.southindia-01.azurewebsites.net/api/parent-admin";
+const API_BASE = "http://localhost:5000/api/parent-admin";
 const STUDENT_API_BASE = "http://localhost:5000/api/student-admin";
 
 export default function AdminParents({ escapeHtml }) {
